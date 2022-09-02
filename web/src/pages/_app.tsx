8d2686071b3d78a50a8981559a1ad579fc7c9a79
Karibash <y.karibayashi@gmail.com>
2022-09-03 00:35:09 +0900

    Add viewport settings

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -2,12 +2,17 @@ import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import { AppProps } from 'next/app';
+import Head from 'next/head';
 
 const cache = createCache({ key: 'css' });
 
 const App = ({ Component, pageProps }: AppProps) => {
+  // noinspection HtmlRequiredTitleElement
   return (
     <CacheProvider value={cache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
       <CssBaseline />
       <Component {...pageProps} />
     </CacheProvider>
