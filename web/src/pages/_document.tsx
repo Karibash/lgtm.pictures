2ba4126ed4286750eb65525b29b6cb4cab9b30eb
Karibash <y.karibayashi@gmail.com>
2022-09-03 00:40:29 +0900

    Set html language setting to Japanese

---
+++ web/src/pages/_document.tsx
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Document = () => {
+  // noinspection HtmlRequiredTitleElement
+  return (
+    <Html lang="ja">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Document;
