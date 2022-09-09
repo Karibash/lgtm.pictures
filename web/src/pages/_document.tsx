f02d9974740cdc777a6d357fe285777175da3a4a
Karibash <y.karibayashi@gmail.com>
2022-09-10 00:35:22 +0900

    Add favicon settings

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -5,9 +5,16 @@ const Document = () => {
   return (
     <Html lang="ja">
       <Head>
+        <link rel="shortcut icon" href="/favicons/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="manifest" href="/favicons/manifest.json" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
+        <meta name="msapplication-TileColor" content="#282a36" />
+        <meta name="theme-color" content="#282a36" />
       </Head>
       <body>
         <Main />
