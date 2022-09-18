7dbe52a3a6ea15197eee487080be8831fea4a5ee
Karibash <y.karibayashi@gmail.com>
2022-09-19 01:48:01 +0900

    Fix bug where tabs could not be transitioned to the screen by key input

--- web/src/components/domains/ApplicationNavigation/index.tsx
+++ web/src/components/domains/ApplicationNavigation/index.tsx
@@ -1,16 +1,20 @@
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useCallback } from 'react';
 
 import TabLink from '../../elements/TabLink';
 
 const ApplicationNavigation: React.FC = () => {
   const router = useRouter();
 
+  const handleOnChange = useCallback((event: React.SyntheticEvent, value: string) => {
+    void router.push(value);
+  }, [router]);
+
   return (
     <Box sx={{ borderBottom: 1, borderColor: 'divider' }} component="nav">
-      <Tabs variant="fullWidth" value={router.pathname}>
+      <Tabs variant="fullWidth" value={router.pathname} onChange={handleOnChange}>
         <TabLink label="LATEST" value="/" href="/" />
         <TabLink label="TRENDS" value="/trends" href="/trends" />
         <TabLink label="UPLOADS" value="/uploads" href="/uploads" />
