7b945e668cafc666cb35a2d98117f4f2fa8b2ca0
Karibash <y.karibayashi@gmail.com>
2022-09-23 00:58:13 +0900

    Make it possible to specify the icon for OpenInNewButton

--- web/src/components/elements/OpenInNewButton/index.tsx
+++ web/src/components/elements/OpenInNewButton/index.tsx
@@ -1,21 +1,25 @@
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import IconButton from '@mui/material/IconButton';
+import { OverridableComponent } from '@mui/material/OverridableComponent';
+import { SvgIconTypeMap } from '@mui/material/SvgIcon/SvgIcon';
 import Tooltip from '@mui/material/Tooltip';
 import React from 'react';
 
 export type OpenInNewButtonProps = {
   href: string;
   size?: 'small' | 'medium' | 'large';
+  icon?: OverridableComponent<SvgIconTypeMap> ;
 };
 
 const OpenInNewButton: React.FC<OpenInNewButtonProps> = ({
   href,
   size,
+  icon: IconComponent = OpenInNewIcon,
 }) => {
   return (
     <Tooltip enterDelay={1000} title="open in new">
       <IconButton target="_blank" rel="noreferrer noopener" href={href} size={size}>
-        <OpenInNewIcon fontSize={size} />
+        <IconComponent fontSize={size} />
       </IconButton>
     </Tooltip>
   );
