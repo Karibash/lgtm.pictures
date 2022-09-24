80006acaa72666e62ce83b59846f05a8cc7bc684
Karibash <y.karibayashi@gmail.com>
2022-09-24 22:41:24 +0900

    Add a link to the GitHub repository in the header

--- web/src/components/domains/ApplicationHeader/index.tsx
+++ web/src/components/domains/ApplicationHeader/index.tsx
@@ -1,15 +1,23 @@
+import GitHubIcon from '@mui/icons-material/GitHub';
 import AppBar from '@mui/material/AppBar';
 import IconButton from '@mui/material/IconButton';
+import Stack from '@mui/material/Stack';
 import { styled, alpha } from '@mui/material/styles';
 import Toolbar from '@mui/material/Toolbar';
 import React from 'react';
 
+import OpenInNewButton from '../../elements/OpenInNewButton';
 import ApplicationLogo from '../ApplicationLogo';
 
 const Wrapper = styled(AppBar)`
-  backdrop-filter: blur(20px);
+  backdrop-filter: blur(18px);
   background-color: ${props => alpha(props.theme.palette.background.default, .76)};
   border-bottom: solid thin ${props => alpha(props.theme.palette.divider, .76)};
+`;
+
+const ExternalLink = styled(OpenInNewButton)`
+  border-radius: 12px;
+  border: solid thin ${props => props.theme.palette.divider};
 `;
 
 export type ApplicationHeaderProps = {
@@ -22,9 +30,18 @@ const ApplicationHeader: React.FC<ApplicationHeaderProps> = ({
   return (
     <Wrapper {...props} elevation={0}>
       <Toolbar>
-        <IconButton color="primary" size="medium" href="/">
-          <ApplicationLogo fontSize="large" />
-        </IconButton>
+        <Stack direction="row" width="100%" alignItems="center" justifyContent="space-between">
+          <IconButton color="primary" size="medium" href="/">
+            <ApplicationLogo fontSize="large" />
+          </IconButton>
+          <Stack direction="row" gap={1.5}>
+            <ExternalLink
+              size="small"
+              href="https://github.com/Karibash/lgtm.pictures"
+              icon={GitHubIcon}
+            />
+          </Stack>
+        </Stack>
       </Toolbar>
     </Wrapper>
   );
