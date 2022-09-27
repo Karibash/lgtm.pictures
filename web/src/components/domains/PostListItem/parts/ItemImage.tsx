74756351d76a3b294a8dd5c4079266ecace2e2ad
Karibash <y.karibayashi@gmail.com>
2022-09-27 23:40:33 +0900

    Make the animation use a transition component

--- web/src/components/domains/PostListItem/parts/ItemImage.tsx
+++ web/src/components/domains/PostListItem/parts/ItemImage.tsx
@@ -1,10 +1,13 @@
+import { Fade } from '@mui/material';
 import Alert from '@mui/material/Alert';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Slide from '@mui/material/Slide';
 import { alpha, styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import { useHover } from 'ahooks';
 import Image from 'next/future/image';
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 
 import { useSnackbar } from '../../../../hooks/useSnackbar';
 import OpenInNewButton from '../../../elements/OpenInNewButton';
@@ -12,53 +15,11 @@ import ViewCount from '../../../elements/ViewCount';
 import PostBlockButton from '../../PostBlockButton';
 
 const Wrapper = styled(Box)`
-  &:after {
-    content: 'Copy';
-    position: absolute;
-    display: flex;
-    opacity: 0;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-    font-size: ${props => props.theme.typography.h5.fontSize};
-    color: ${props => props.theme.palette.getContrastText(props.theme.palette.background.default)};
-    transition: ${props => {
-      return props.theme.transitions.create(['opacity', 'background-color'], {
-        duration: props.theme.transitions.duration.short,
-      });
-    }};
-  }
-
-  &:hover {
-    &:after {
-      opacity: 1;
-      pointer-events: none;
-      background-color: ${props => alpha(props.theme.palette.background.default, .48)};
-    }
-
-    > .header {
-      opacity: 1;
-    }
-
-    > .footer {
-      opacity: 1;
-      transform: translateY(0%) scaleY(100%);
-    }
-  }
-
-  .event {
-    pointer-events: auto;
-  }
-`;
-
-const ImageWrapper = styled(Button)`
   position: relative;
   display: block;
   padding: 0;
   width: 100%;
+  overflow: hidden;
 
   img {
     display: block;
@@ -68,29 +29,51 @@ const ImageWrapper = styled(Button)`
   }
 `;
 
+const Overlay = styled(Box)`
+  position: absolute;
+  display: grid;
+  place-items: center;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: ${props => alpha(props.theme.palette.background.default, .48)};
+
+  .MuiButton-root {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    color: ${props => props.theme.palette.text.primary};
+    font-size: ${props => props.theme.typography.h5.fontSize};
+
+    &:hover {
+      background-color: transparent;
+    }
+
+    .MuiTouchRipple-root {
+      color: ${props => props.theme.palette.primary.main};
+    }
+  }
+`;
+
 const Header = styled(Box)`
   position: absolute;
-  z-index: 1;
-  opacity: 0;
   top: 0;
-  right: 0;
+  left: 0;
   width: 100%;
   padding: 4px;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   pointer-events: none;
-  transition: ${props => {
-    return props.theme.transitions.create(['opacity'], {
-      duration: props.theme.transitions.duration.short,
-    });
-  }};
+
+  > * {
+    pointer-events: auto;
+  }
 `;
 
 const Footer = styled(Box)`
   position: absolute;
-  z-index: 1;
-  opacity: 1;
   left: 0;
   bottom: 0;
   width: 100%;
@@ -102,12 +85,10 @@ const Footer = styled(Box)`
   justify-content: space-between;
   pointer-events: none;
   background-color: ${props => alpha(props.theme.palette.background.default, .48)};
-  transform: translateY(50%) scaleY(0%);
-  transition: ${props => {
-    return props.theme.transitions.create(['opacity', 'transform'], {
-      duration: props.theme.transitions.duration.short,
-    });
-  }};
+
+  > * {
+    pointer-events: auto;
+  }
 `;
 
 export type ItemImageProps = {
@@ -126,6 +107,8 @@ const ItemImage: React.FC<ItemImageProps> = ({
   image,
   viewCount,
 }) => {
+  const ref = useRef<HTMLDivElement>();
+  const isHovering = useHover(ref);
   const snackbar = useSnackbar();
 
   const handleClickImage = useCallback(() => {
@@ -138,22 +121,31 @@ const ItemImage: React.FC<ItemImageProps> = ({
   }, [image.url, snackbar]);
 
   return (
-    <Wrapper position="relative">
-      <ImageWrapper onClick={handleClickImage}>
-        <Image
-          src={image.url}
-          alt={image.title}
-          width={image.width}
-          height={image.height}
-        />
-      </ImageWrapper>
-      <Header className="header">
-        <PostBlockButton className="event" size="small" />
-        <OpenInNewButton className="event" size="small" title="image url" href={`/posts/${id}`} />
-      </Header>
-      <Footer className="footer">
-        <ViewCount className="event" size="small" count={viewCount} />
-      </Footer>
+    <Wrapper ref={ref}>
+      <Image
+        src={image.url}
+        alt={image.title}
+        width={image.width}
+        height={image.height}
+      />
+      <Fade in={isHovering}>
+        <Box>
+          <Overlay>
+            <Button onClick={handleClickImage}>
+              Copy
+            </Button>
+          </Overlay>
+          <Header>
+            <PostBlockButton size="small" />
+            <OpenInNewButton size="small" title="image url" href={`/posts/${id}`} />
+          </Header>
+          <Slide direction="up" in={isHovering} container={ref.current}>
+            <Footer>
+              <ViewCount size="small" count={viewCount} />
+            </Footer>
+          </Slide>
+        </Box>
+      </Fade>
     </Wrapper>
   );
 };
